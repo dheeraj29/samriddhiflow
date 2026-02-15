a19b081841fd080948f64106e7aca6333e223de1
Dheeraj Arumalla <dheeraj.arumalla@gmail.com>
2026-02-15 23:59:08 +0530

    updating version to 2.3.0

--- lib/core/app_constants.dart
+++ lib/core/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
   static const String appName = 'Samriddhi Flow';
   static const String appTagline = 'Your Premium Finance Companion';
-  static const String appVersion = 'v2.2.0';
+  static const String appVersion = 'v2.3.0';
 }
